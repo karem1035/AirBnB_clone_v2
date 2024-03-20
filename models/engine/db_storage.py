6b7a625b7839d02cb9d16dedd527587e0ed864bd
michael-shambel <Michaelshambel10@gmail.com>
2024-03-20 09:35:01 -0700

    tas 6 engine

---
+++ models/engine/db_storage.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" database for file class """
+from sqlalchemy import create_engine
+form sqlalchemy.orm import sessiomaker, scoped_session
+from os import getenv
+from models.base_model import Base
+
+
+class DBStorage:
+    """ create a table """
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """ create a database connection """
+        user=getenv("HBNB_MYSQL_USER")
+        passwd=getenv("HBNB_MYSQL_PWD")
+        host=getenv("HBNB_MYSQL_HOST")
+        database=getenv("HBNB_MYSQL_DB")
+        env=getenv("HBNB_ENV")
+
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}:3306/{}'
+                .format(user, passwd, host, database), pool_pre_ping=TRUE)
+        if env == 'test':
+            Base.metadata.drop_all(self.__engine)
