0b439acb50982994fbda6423785638097e8dc07d
michael-shambel <Michaelshambel10@gmail.com>
2024-03-21 11:04:40 -0700

    task 6 pycode

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -20,21 +20,23 @@ class DBStorage:
 
     def __init__(self):
         """ create a database connection """
-        user=getenv("HBNB_MYSQL_USER")
-        passwd=getenv("HBNB_MYSQL_PWD")
-        host=getenv("HBNB_MYSQL_HOST")
-        database=getenv("HBNB_MYSQL_DB")
-        env=getenv("HBNB_ENV")
+        user = getenv("HBNB_MYSQL_USER")
+        passwd = getenv("HBNB_MYSQL_PWD")
+        host = getenv("HBNB_MYSQL_HOST")
+        database = getenv("HBNB_MYSQL_DB")
+        env = getenv("HBNB_ENV")
 
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                .format(user, passwd, host, database), pool_pre_ping=True)
+                                      .format(user, passwd, host, database),
+                                      pool_pre_ping=True)
         if env == 'test':
             Base.metadata.drop_all(self.__engine)
+
     def all(self, cls=None):
         """ return the dictionary of an object """
-        objects= {}
+        objects = {}
         if cls:
-            query =self.__session.query(cls)
+            query = self.__session.query(cls)
             for element in query:
                 key = "{}.{}".format(type(element).__name__, element.id)
                 objects[key] = element
@@ -45,22 +47,27 @@ class DBStorage:
                 key = "{}.{}".format(type(obj).__name__, obj.id)
                 objects[key] = obj
         return (objects)
+
     def new(self, obj):
         """Adds the object to the current database session"""
         self.__session.add(obj)
+
     def save(self):
         """Commits all changes of the current database session"""
         self.__session.commit()
+
     def delete(self, obj=None):
         """Deletes obj from the current database session"""
         if obj:
             self.__session.delete(obj)
+
     def reload(self):
         """Creates all tables in the database"""
         Base.metadata.create_all(self.__engine)
         Session = scoped_session(sessionmaker(bind=self.__engine,
-            expire_on_commit=False))
+                                 expire_on_commit=False))
         self.__session = Session()
+
     def close(self):
         """calls remove()"""
         self.__session.close()
