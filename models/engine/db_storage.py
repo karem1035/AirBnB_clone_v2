b251eedc8db40844416fe9c3a3a8c79571408b22
karem darwesh <karemdarwesh1035@gmail.com>
2024-04-27 21:20:26 +0300

    task 7

---
+++ models/engine/db_storage.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+""" This modules for Database Storage """
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import BaseModel, Base
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+import os
+
+classes = {
+    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+    'State': State, 'City': City, 'Amenity': Amenity,
+    'Review': Review
+}
+
+
+class DBStorage:
+    '''
+    Handles database engine
+    '''
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+            getenv('HBNB_MYSQL_USER'),
+            getenv('HBNB_MYSQL_PWD'),
+            getenv('HBNB_MYSQL_HOST'),
+            getenv('HBNB_MYSQL_DB')),
+            pool_pre_ping=True
+        )
+        if getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """ all method """
+        dict = {}
+        for clss in classes:
+            if cls is None or cls is classes[clss] or cls is clss:
+                objs = self.__session.query(classes[clss]).all()
+                for obj in objs:
+                    key = obj.__class__.__name__ + '.' + obj.id
+                    dict[key] = obj
+        return (dict)
+
+    def new(self, obj):
+        """Add the object to the current database session."""
+        self.__session.add(obj)
+
+    def save(self):
+        """ save method """
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """ delete method """
+        if obj:
+            self.__session.delete(obj)
+            self.save()
+
+    def reload(self):
+        """ reload method """
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
